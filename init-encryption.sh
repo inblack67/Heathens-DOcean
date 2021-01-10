f8deb6878c0d94d67bc7f3dda9e8826026c3de70
Aman Bhardwaj <inblack>
2021-01-10 21:19:28 +0530

    ssl domain

--- init-encryption.sh
+++ init-encryption.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(heathens.tk www.heathens.tk)
+domains=(21heathens.tk www.21heathens.tk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="devproddeploy@gmail.com" # Adding a valid address is strongly recommended
