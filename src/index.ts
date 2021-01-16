a783d7769ffdddef5e385dc7de2f9432e47ea087
Aman Bhardwaj <inblack>
2021-01-16 20:40:29 +0530

    hack

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ const main = async () => {
             const customError = errorFormatter(err);
             return customError;
         },
-        playground: false,
+        playground: true,
     });
 
     apolloServer.installSubscriptionHandlers(ws);
